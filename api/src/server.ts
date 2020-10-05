1d26c0a962f9072e5199cfab058e9fb3dc3cc589
Karol Goraus <karol.goraus@thesoftwarehouse.pl>
2020-10-05 08:55:43 +0200

    [WIP] Add prisma client

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,9 @@
 import { createContainer } from "./container";
 import Koa from "koa";
+import { PrismaClient } from ".prisma/client";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const { PrismaClient } = require("@prisma/client");
 
 (async () => {
   const container = await createContainer();
@@ -7,9 +11,29 @@ import Koa from "koa";
   const app = container.resolve<Koa>("app");
   const port = process.env.APP_PORT;
 
+  const prisma: PrismaClient = new PrismaClient();
+  const stats = await prisma.playersRelativeDPSStats.findMany({
+    take: 10,
+    select: {
+      Id: true,
+      CharacterName: true,
+      AccountName: true,
+      Profession: true,
+      FightName: true,
+      TargetDps: true,
+      TotalDps: true,
+    },
+    where: {
+      FightName: {
+        contains: "Cairn",
+      },
+      Profession: "Druid",
+    },
+  });
+
+  console.log(stats);
+
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
   });
-
-  console.log("HELLO");
 })();
